efa5f29b1467871ebc1e9ffc3925bd7f2d101ee4
Yt1m <ytimrdonqaz@gmail.com>
2023-10-15 19:09:20 +0200

    Добавьте файлы проекта.

---
+++ Условные операторы/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("Введите первую сторону треугольника");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите вторую сторону треугольника");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третью сторону треугольника");
+int c = int.Parse(Console.ReadLine());
+
+if ((a < b + c) ^ (b < a + c) ^ (c < a + b))
+{
+    Console.WriteLine("Возможно");
+}
+else Console.WriteLine("Невозможно");
